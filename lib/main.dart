c50942b822f7d01ab6933b09d13c174464fcc5e4
Milen Dimitrov <milen.dimitrov@paysafe.com>
2022-11-06 13:26:38 +0200

    MVP

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:noise_meter/noise_meter.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,105 +15,132 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ITIDO Noise Meter',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: const Color(0xFF002554),
+          secondary: const Color.fromARGB(255, 138, 71, 114),
+          brightness: Brightness.light,
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'ITIDO Noise Meter'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _NoiseMeterHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _NoiseMeterHomePageState extends State<MyHomePage> {
+  final int _laudNoiseDB = 81;
+  final int _mediumNoiseDB = 75;
+  final int _lowNoiseDB = 68;
+  bool _isRecording = false;
+  String _noiseLevel = "";
+  late StreamSubscription<NoiseReading> _noiseSubscription;
+  late final NoiseMeter _noiseMeter;
+  late AssetsAudioPlayer _audio;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _noiseMeter = NoiseMeter(_onNoiseInitError);
+    _startNoiseListening();
+
+    _audio = AssetsAudioPlayer.newPlayer();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _stopNoiseListening();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Noise value:',
             ),
             Text(
-              '$_counter',
+              _noiseLevel,
               style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void _startNoiseListening() async {
+    try {
+      _noiseSubscription = _noiseMeter.noiseStream.listen(_onNoiseData);
+    } catch (exception) {
+      debugPrint(exception.toString());
+    }
+  }
+
+  void _onNoiseData(NoiseReading noiseReading) {
+    debugPrint(
+        "onNoiseData: ${noiseReading.meanDecibel} is playing: ${_audio.isPlaying.value}");
+    if (!_audio.isPlaying.value) {
+      var data = noiseReading.meanDecibel;
+      var intDB = data.isInfinite ? 0 : data.toInt();
+
+      setState(() {
+        if (!_isRecording) {
+          _isRecording = true;
+        }
+
+        _noiseLevel = '$intDB';
+
+        _checkShouldAllert(intDB);
+      });
+    }
+  }
+
+  void _stopNoiseListening() async {
+    try {
+      _noiseSubscription.cancel();
+      setState(() => _isRecording = false);
+    } catch (e) {
+      debugPrint('stopRecorder error: $e');
+    }
+  }
+
+  void _onNoiseInitError(Object error) {
+    setState(() {
+      _isRecording = false;
+      _noiseLevel = "intialization Error";
+    });
+  }
+
+  void _checkShouldAllert(int intDB) {
+    if (intDB > _laudNoiseDB) {
+      _play('assets/mp3/to_much_noise.mp3', 1.0);
+    } else if (intDB > _mediumNoiseDB) {
+      _play('assets/mp3/it_is_noisy.mp3', 1.0);
+    } else if (intDB > _lowNoiseDB) {
+      _play('assets/mp3/it_is_noisy.mp3', 0.5);
+    }
+  }
+
+  void _play(String source, double volume) {
+    _audio.open(
+      Audio(source),
+      volume: volume,
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:noise_meter/main.dart';
+import 'package:itido_noise_meter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
